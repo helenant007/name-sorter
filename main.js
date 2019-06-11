23dcb9d6580ae6f86643b9c8b667cbe051403ce8
Helena Natanael <helena.natanael@tokopedia.com>
2019-06-12 01:01:52 +0700

    write output to file

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ const sorter = require("./src/sorter");
 
 const fileName = process.argv[2];
 const fileContent = fs.readFileSync(fileName).toString();
-console.log(fileContent);
 const listOfNames = fileContent.split("\n").filter(name => name);
 
 const result = sorter(listOfNames);
-console.log(result);
+const formatResult = result.join("\n");
+console.log(formatResult);
+
+fs.writeFileSync("sorted-names-list.txt", formatResult);
