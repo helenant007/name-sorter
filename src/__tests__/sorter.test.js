b218e503390f5f626c036c16e4c773b4b8b57d42
Helena Natanael <helena.natanael@tokopedia.com>
2019-06-12 01:01:44 +0700

    add sorting logic and sorting tests

--- src/__tests__/sorter.test.js
+++ src/__tests__/sorter.test.js
@@ -1,39 +1,75 @@
 const sorter = require("../sorter");
 
-describe("test sorting names", () => {
-    test("sorting", () => {
+describe("Sorting Names: Positive Test Cases", () => {
+    test("Sorting Names", () => {
         const unsortedNames = [
-            Orson Milka Iddins,
-            Erna Dorey Battelle,
-            Flori Chaunce Franzel,
-            Odetta Sue Kaspar,
-            Roy Ketti Kopfen,
-            Madel Bordie Mapplebeck,
-            Selle Bellison,
-            Leonerd Adda Mitchell Monaghan,
-            Debra Micheli,
-            Hailey Avie Annakin
+            "Orson Milka Iddins",
+            "Erna Dorey Battelle",
+            "Flori Chaunce Franzel",
+            "Odetta Sue Kaspar",
+            "Roy Ketti Kopfen",
+            "Madel Bordie Mapplebeck",
+            "Selle Bellison",
+            "Leonerd Adda Mitchell Monaghan",
+            "Debra Micheli",
+            "Hailey Avie Annakin",
         ];
-
         const expectedResult = [
-            Hailey Avie Annakin,
-            Erna Dorey Battelle,
-            Selle Bellison,
-            Flori Chaunce Franzel,
-            Orson Milka Iddins,
-            Odetta Sue Kaspar,
-            Roy Ketti Kopfen,
-            Madel Bordie Mapplebeck,
-            Debra Micheli,
-            Leonerd Adda Mitchell Monaghan
+            "Hailey Avie Annakin",
+            "Erna Dorey Battelle",
+            "Selle Bellison",
+            "Flori Chaunce Franzel",
+            "Orson Milka Iddins",
+            "Odetta Sue Kaspar",
+            "Roy Ketti Kopfen",
+            "Madel Bordie Mapplebeck",
+            "Debra Micheli",
+            "Leonerd Adda Mitchell Monaghan",
         ];
-
         const actualResult = sorter(unsortedNames);
-
         expect(actualResult).toEqual(expectedResult);
     });
+});
 
-    test("sorting empty array", () => {});
+describe("Sorting Names: Negative Test Cases", () => {
+    test("sorting empty array", () => {
+        const emptyArr = [];
 
-    test("sorting names that has number or invalid char", () => {});
+        const expectedResult = Error;
+
+        expect(() => {
+            extractLastName(emptyArr);
+        }).toThrow(expectedResult);
+    });
+
+    test("sorting names that has number or invalid char", () => {
+        const unsortedNames = [
+            "Erna Dorey Battelle",
+            "Flori Chaunce Franzel",
+            "Odetta Sue Kaspar",
+            "Roy Ketti Kopfen",
+            "Erna Dorey B???ttelle",
+            "Madel Bordie Mapplebeck",
+            "Selle Bellison",
+            "Leonerd Adda Mitchell Monaghan",
+            "Debra Micheli",
+            "Erna Dorey B12345ttelle",
+            "Hailey Avie Annakin",
+        ];
+        const expectedResult = [
+            "Hailey Avie Annakin",
+            "Erna Dorey B???ttelle",
+            "Erna Dorey B12345ttelle",
+            "Erna Dorey Battelle",
+            "Selle Bellison",
+            "Flori Chaunce Franzel",
+            "Odetta Sue Kaspar",
+            "Roy Ketti Kopfen",
+            "Madel Bordie Mapplebeck",
+            "Debra Micheli",
+            "Leonerd Adda Mitchell Monaghan",
+        ];
+        const actualResult = sorter(unsortedNames);
+        expect(actualResult).toEqual(expectedResult);
+    });
 });
--- src/sorter.js
+++ src/sorter.js
@@ -1,5 +1,20 @@
+const extractLastName = require("./extractLastName");
+
 function sort(listOfNames) {
     const sortedNames = listOfNames;
+    for (let i = 0; i < listOfNames.length; i++) {
+        for (let j = 0; j + i < listOfNames.length - 1; j++) {
+            const firstLastName = extractLastName(sortedNames[j]);
+            const secondLastName = extractLastName(sortedNames[j + 1]);
+
+            if (firstLastName.localeCompare(secondLastName) === 1) {
+                [sortedNames[j], sortedNames[j + 1]] = [
+                    sortedNames[j + 1],
+                    sortedNames[j],
+                ];
+            }
+        }
+    }
 
     return sortedNames;
 }
